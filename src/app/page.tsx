a80c9b89cac99c27af9d1be89a2dad705711a066
VRMX2 <lehcengrissi@gmail.com>
2025-10-19 15:08:33 -0700

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
-const HomePage = ()=>{
-	return <div>Home Page</div>
+"use client";
+
+import { SignedOut,SignedIn,SignInButton } from "@clerk/clerk-react";
+import {SignOutButton} from "@clerk/nextjs";
+
+const HomePage = () => {
+	return (
+		<div>
+			Home Page
+			<SignedOut>
+				<SignInButton />
+			</SignedOut>
+			<SignedIn>
+				<SignOutButton />
+			</SignedIn>
+		</div>
+	);
 };
 
 export default HomePage;
