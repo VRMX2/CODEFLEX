6ac27c1abcc4aa42cba53360132893c91e4a3198
VRMX2 <lehcengrissi@gmail.com>
2025-10-19 13:41:34 -0700

    new commit

---
+++ src/app/generate-program/page.tsx
@@ -0,0 +1,5 @@
+const GenerateProgramPage = (){
+    return <div>Generate Program Page</div>
+}
+export default GenerateProgramPage;
+
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,4 @@
+const ProfilePage = ()=>{
+	return <div>Welcome to Profile page</div>
+}
+export default ProfilePage;
